436b32e891c4f786cb207cf6ed8d347a338ea9b4
Jai <80166407+jairajput@users.noreply.github.com>
2023-04-22 15:48:57 +0530

    first commit

---
+++ ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jai  on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
---
+++ TrailingIconLabelStyle.swift..swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift..swift
+//  Scrumdinger
+//
+//  Created by Jai  on 22/04/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
